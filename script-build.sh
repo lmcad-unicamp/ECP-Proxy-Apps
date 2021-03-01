c2c20e810fa29230870451ff9a66922850886a63
thais <thaiscamachoo@gmail.com>
2021-03-01 16:58:16 -0300

    up

--- script-build.sh
+++ script-build.sh
@@ -1,30 +1,37 @@
 #!/bin/bash
 
 # Laghos-hypre
-cd hypre/src
-./configure --disable-fortran
-make -j2
-cd ../..
+#cd hypre/src
+#./configure --disable-fortran
+#make -j2
+#cd ../..
+#
+## Laghos-metis
+#cd metis-4.0.3
+#make -j2
+#cd ..
+#
+## Laghos-mfem
+#cd mfem
+#make parallel -j2
+#cd ..
+#
+## Laghos
+#cd Laghos
+#make
+#cd ..
+#
+#cd AMG
+#make
+#cd ..
 
-# Laghos-metis
-cd metis-4.0.3
-make -j2
-cd ..
+export THORNADO_DIR=$HOME/Dev/master/PI-Bench/ECP-Proxy-Apps/thornado_mini
+export THORNADO_MACHINE=mymachine
 
-# Laghos-mfem
-cd mfem
-make parallel -j2
-cd ..
-
-# Laghos
-cd Laghos
+cd thornado_mini/DeleptonizationProblem/Executables
 make
-cd ..
+mkdir ../Output
+cd ../../..
 
-cd AMG
-make
-cd ..
-
-cd XSBench/openmp-threading
-make
-cd ../..
+#cd XSBench/openmp-threading
+#make
--- script-exec.sh
+++ script-exec.sh
@@ -7,10 +7,17 @@ cd Laghos
 /usr/bin/time -v ./laghos -p 3 -m data/rectangle01_quad.mesh -rs 2 -tf 5.0 -pa &> ../exp-results/laghos.out
 cd ..
 
+# AMG
 cd AMG
 /usr/bin/time -v ./test/amg -problem 2 &> ../exp-results/amg.out
 cd ..
 
+# XSBench
 cd XSBench/openmp-threading
 /usr/bin/time -v ./XSBench -G nuclide -s large &> ../exp-results/xsbench.out
 cd ../..
+
+# THORNADO-MINI
+cd thornado_mini/DeleptonizationProblem/Executables
+/usr/bin/time -v ./DeleptonizationProblem1D_mymachine -nX_Option=100
+cd ../../..
