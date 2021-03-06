2d55f2b6d5807e82b70de7daff632cdd54ecdb63
thais <thaiscamachoo@gmail.com>
2021-03-06 02:24:24 -0300

    up

--- script-build.sh
+++ script-build.sh
@@ -3,31 +3,31 @@
 export THORNADO_DIR=$1
 export THORNADO_MACHINE=mymachine
 
-## Laghos-hypre
-#cd hypre/src
-#./configure --disable-fortran
-#make -j2
-#cd ../..
-#
-## Laghos-metis
-#cd metis-4.0.3
-#make -j2
-#cd ..
-#
-## Laghos-mfem
-#cd mfem
-#make parallel -j2
-#cd ..
-#
-## Laghos
-#cd Laghos
-#make
-#cd ..
-#
-## AMG
-#cd AMG
-#make
-#cd ..
+# Laghos-hypre
+cd hypre/src
+./configure --disable-fortran
+make -j2
+cd ../..
+
+# Laghos-metis
+cd metis-4.0.3
+make -j2
+cd ..
+
+# Laghos-mfem
+cd mfem
+make parallel -j2
+cd ..
+
+# Laghos
+cd Laghos
+make
+cd ..
+
+# AMG
+cd AMG
+make
+cd ..
 
 # THORNADO-MINI
 cd thornado_mini/DeleptonizationProblem/Executables
@@ -37,63 +37,63 @@ cp ../../../thornado-tables/mini-app/wl-Op-SFHo-15-25-50-noBCK.h5 OpacityTable.h
 mkdir ../Output
 cd ../../..
 
-## XSBench
-#cd XSBench/openmp-threading
-#make
-#cd ../..
-#
-## Ember
-#cd ember/mpi/halo3d
-#make
-#cd ../../..
-#
-## ExaMiniMD
-#cd ExaMiniMD
-#make
-#cd ..
-#
-## MACSio
-#cd MACSio
-#mkdir build
-#cd build
-#cmake ..
-#make
-#cd ../..
-#
-## miniAMR
-#cd miniAMR/openmp
-#make
-#cd ../..
-#
-## miniqmc
-#cd miniqmc
-#mkdir build
-#cd build
-#cmake -D CMAKE_CXX_COMPILER=mpic++ .. -D ENABLE_OPENMP=OFF ..
-#make
-#cd ../..
-#
-## miniVite
-#cd miniVite
-#make
-#cd ..
-#
-## Nekbone
-#cd Nekbone/test/example1/
-#./makenek
-#cd ../../..
-#
-## sw4lite
-#cd sw4lite
-#make
-#cd ..
-#
-## SWFFT
-#cd SWFFT
-#make
-#cd ..
-#
-## PICSAR
-#cd PICSAR
-#make
-#cd ..
+# XSBench
+cd XSBench/openmp-threading
+make
+cd ../..
+
+# Ember
+cd ember/mpi/halo3d
+make
+cd ../../..
+
+# ExaMiniMD
+cd ExaMiniMD
+make
+cd ..
+
+# MACSio
+cd MACSio
+mkdir build
+cd build
+cmake ..
+make
+cd ../..
+
+# miniAMR
+cd miniAMR/openmp
+make
+cd ../..
+
+# miniqmc
+cd miniqmc
+mkdir build
+cd build
+cmake -D CMAKE_CXX_COMPILER=mpic++ .. -D ENABLE_OPENMP=OFF ..
+make
+cd ../..
+
+# miniVite
+cd miniVite
+make
+cd ..
+
+# Nekbone
+cd Nekbone/test/example1/
+./makenek
+cd ../../..
+
+# sw4lite
+cd sw4lite
+make
+cd ..
+
+# SWFFT
+cd SWFFT
+make
+cd ..
+
+# PICSAR
+cd PICSAR
+make
+cd ..
