79a721a4a539d3ec5aa8255025b479e16f568c58
thais <thaiscamachoo@gmail.com>
2021-03-06 16:06:58 -0300

    up

--- script-build.sh
+++ script-build.sh
@@ -7,29 +7,29 @@ export SOURCE_ROOT=$PATH_DIR/Nekbone/src
 export KOKKOS_PATH=${PATH_DIR}/kokkos
 export THORNADO_MACHINE=mymachine
 
-# Kokkos
-cd kokkos
-mkdir build
-cd build
-cmake ..
-make
-cd $PATH_DIR
-
-# Laghos-hypre
-cd hypre/src
-./configure --disable-fortran
-make -j2
-cd $PATH_DIR
-
-# Laghos-metis
-cd metis-4.0.3
-make -j2
-cd $PATH_DIR
-
-# Laghos-mfem
-cd mfem
-make parallel -j2
-cd $PATH_DIR
+## Kokkos
+#cd kokkos
+#mkdir build
+#cd build
+#cmake ..
+#make
+#cd $PATH_DIR
+#
+## Laghos-hypre
+#cd hypre/src
+#./configure --disable-fortran
+#make -j2
+#cd $PATH_DIR
+#
+## Laghos-metis
+#cd metis-4.0.3
+#make -j2
+#cd $PATH_DIR
+#
+## Laghos-mfem
+#cd mfem
+#make parallel -j2
+#cd $PATH_DIR
 
 # Laghos
 cd Laghos
