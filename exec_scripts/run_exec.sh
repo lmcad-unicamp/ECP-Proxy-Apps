fc2de34d74f4f91cb45b8140f180aeadec9416c2
Thais <thaiscamachoo@gmail.com>
2021-06-18 23:12:25 -0300

    up

--- exec_scripts/run_exec.sh
+++ exec_scripts/run_exec.sh
@@ -100,67 +100,68 @@ mpirun -n $2 --hostfile $HOSTFILE $picsar_b_app > $DATASET/$NAME-picsar-B.out 2>
 mpirun -n $2 --hostfile $HOSTFILE $picsar_c_app > $DATASET/$NAME-picsar-C.out 2> $DATASET/$NAME-picsar-C.err
 cd ..
 # AMG
-#cd AMG
-#mpirun -n $2 --hostfile $HOSTFILE $amg_a_app > $DATASET/$NAME-amg-A.out 2> $DATASET/$NAME-amg-A.err
-#cd ..
-## Laghos
-#cd Laghos
-#mpirun -n $2 --hostfile $HOSTFILE $laghos_a_app > $DATASET/$NAME-laghos-A.out 2> $DATASET/$NAME-laghos-A.err
-#mpirun -n $2 --hostfile $HOSTFILE $laghos_b_app > $DATASET/$NAME-laghos-B.out 2> $DATASET/$NAME-laghos-B.err
-#mpirun -n $2 --hostfile $HOSTFILE $laghos_c_app > $DATASET/$NAME-laghos-C.out 2> $DATASET/$NAME-laghos-C.err
-#cd ..
+cd AMG
+mpirun -n $2 --hostfile $HOSTFILE $amg_a_app > $DATASET/$NAME-amg-A.out 2> $DATASET/$NAME-amg-A.err
+cd ..
+# Laghos
+cd Laghos
+mpirun -n $2 --hostfile $HOSTFILE $laghos_a_app > $DATASET/$NAME-laghos-A.out 2> $DATASET/$NAME-laghos-A.err
+mpirun -n $2 --hostfile $HOSTFILE $laghos_b_app > $DATASET/$NAME-laghos-B.out 2> $DATASET/$NAME-laghos-B.err
+mpirun -n $2 --hostfile $HOSTFILE $laghos_c_app > $DATASET/$NAME-laghos-C.out 2> $DATASET/$NAME-laghos-C.err
+cd ..
 # XSBench
-#cd XSBench/openmp-threading
-#mpirun -n $2 --hostfile $HOSTFILE $xsbench_a_app > $DATASET/$NAME-xsbench-A.out 2> $DATASET/$NAME-xsbench-A.err
-#mpirun -n $2 --hostfile $HOSTFILE $xsbench_b_app > $DATASET/$NAME-xsbench-B.out 2> $DATASET/$NAME-xsbench-B.err
-#mpirun -n $2 --hostfile $HOSTFILE $xsbench_c_app > $DATASET/$NAME-xsbench-C.out 2> $DATASET/$NAME-xsbench-C.err
-#cd ../..
+cd XSBench/openmp-threading
+mpirun -n $2 --hostfile $HOSTFILE $xsbench_a_app > $DATASET/$NAME-xsbench-A.out 2> $DATASET/$NAME-xsbench-A.err
+mpirun -n $2 --hostfile $HOSTFILE $xsbench_b_app > $DATASET/$NAME-xsbench-B.out 2> $DATASET/$NAME-xsbench-B.err
+mpirun -n $2 --hostfile $HOSTFILE $xsbench_c_app > $DATASET/$NAME-xsbench-C.out 2> $DATASET/$NAME-xsbench-C.err
+cd ../..
 # THORNADO-MINI
-#cd thornado_mini/DeleptonizationProblem/Executables
-#mpirun -n $2 --hostfile $HOSTFILE $thornado_mini_a_app > $DATASET/$NAME-thornado-mini-A.out 2> $DATASET/$NAME-thornado-mini-A.err
-#cd ../../..
-#cd ember/mpi/halo3d
-#mpirun -n $2 --hostfile $HOSTFILE $ember_a_app > $DATASET/$NAME-ember-A.out 2> $DATASET/$NAME-ember-A.err
-#mpirun -n $2 --hostfile $HOSTFILE $ember_b_app > $DATASET/$NAME-ember-B.out 2> $DATASET/$NAME-ember-B.err
-#mpirun -n $2 --hostfile $HOSTFILE $ember_c_app > $DATASET/$NAME-ember-C.out 2> $DATASET/$NAME-ember-C.err
-#cd ../../..
+cd thornado_mini/DeleptonizationProblem/Executables
+mpirun -n $2 --hostfile $HOSTFILE $thornado_mini_a_app > $DATASET/$NAME-thornado-mini-A.out 2> $DATASET/$NAME-thornado-mini-A.err
+cd ../../..
+# ember
+cd ember/mpi/halo3d
+mpirun -n $2 --hostfile $HOSTFILE $ember_a_app > $DATASET/$NAME-ember-A.out 2> $DATASET/$NAME-ember-A.err
+mpirun -n $2 --hostfile $HOSTFILE $ember_b_app > $DATASET/$NAME-ember-B.out 2> $DATASET/$NAME-ember-B.err
+mpirun -n $2 --hostfile $HOSTFILE $ember_c_app > $DATASET/$NAME-ember-C.out 2> $DATASET/$NAME-ember-C.err
+cd ../../..
 # ExaMiniMD
-#cd ExaMiniMD
-#mpirun -n $2 --hostfile $HOSTFILE $examinimd_a_app > $DATASET/$NAME-examinimd-A.out 2> $DATASET/$NAME-examinimd-A.err
-#mpirun -n $2 --hostfile $HOSTFILE $examinimd_b_app > $DATASET/$NAME-examinimd-B.out 2> $DATASET/$NAME-examinimd-B.err
-#mpirun -n $2 --hostfile $HOSTFILE $examinimd_c_app > $DATASET/$NAME-examinimd-C.out 2> $DATASET/$NAME-examinimd-C.err
-#cd ..
+cd ExaMiniMD
+mpirun -n $2 --hostfile $HOSTFILE $examinimd_a_app > $DATASET/$NAME-examinimd-A.out 2> $DATASET/$NAME-examinimd-A.err
+mpirun -n $2 --hostfile $HOSTFILE $examinimd_b_app > $DATASET/$NAME-examinimd-B.out 2> $DATASET/$NAME-examinimd-B.err
+mpirun -n $2 --hostfile $HOSTFILE $examinimd_c_app > $DATASET/$NAME-examinimd-C.out 2> $DATASET/$NAME-examinimd-C.err
+cd ..
 # MACSio
-#cd MACSio/build
-#mpirun -n $2 --hostfile $HOSTFILE $macsio_a_app > $DATASET/$NAME-macsio-A.out 2> $DATASET/$NAME-macsio-A.err
-#mpirun -n $2 --hostfile $HOSTFILE $macsio_b_app > $DATASET/$NAME-macsio-B.out 2> $DATASET/$NAME-macsio-B.err
-#mpirun -n $2 --hostfile $HOSTFILE $macsio_c_app > $DATASET/$NAME-macsio-C.out 2> $DATASET/$NAME-macsio-C.err
-#cd ../..
+cd MACSio/build
+mpirun -n $2 --hostfile $HOSTFILE $macsio_a_app > $DATASET/$NAME-macsio-A.out 2> $DATASET/$NAME-macsio-A.err
+mpirun -n $2 --hostfile $HOSTFILE $macsio_b_app > $DATASET/$NAME-macsio-B.out 2> $DATASET/$NAME-macsio-B.err
+mpirun -n $2 --hostfile $HOSTFILE $macsio_c_app > $DATASET/$NAME-macsio-C.out 2> $DATASET/$NAME-macsio-C.err
+cd ../..
 # miniAMR
-#cd miniAMR/openmp
-#mpirun -n $2 --hostfile $HOSTFILE $miniamr_a_app > $DATASET/$NAME-miniamr-A.out 2> $DATASET/$NAME-miniamr-A.err
-#cd ../..
-## miniqmc
-#cd miniqmc/build
-#mpirun -n $2 --hostfile $HOSTFILE $miniqmc_a_app > $DATASET/$NAME-miniqmc-A.out 2> $DATASET/$NAME-miniqmc-A.err
-#mpirun -n $2 --hostfile $HOSTFILE $miniqmc_b_app > $DATASET/$NAME-miniqmc-B.out 2> $DATASET/$NAME-miniqmc-B.err
-#mpirun -n $2 --hostfile $HOSTFILE $miniqmc_c_app > $DATASET/$NAME-miniqmc-C.out 2> $DATASET/$NAME-miniqmc-C.err
-#cd ../..
-## miniVite
-#cd miniVite
-#mpirun -n $2 --hostfile $HOSTFILE $minivite_a_app > $DATASET/$NAME-minivite-A.out 2> $DATASET/$NAME-minivite-A.err
-#cd ..
-## Nekbone
-#cd Nekbone/test/example1/
-#mpirun -n $2 --hostfile $HOSTFILE $nekbone_a_app > $DATASET/$NAME-nekbone-A.out 2> $DATASET/$NAME-nekbone-A.err
-#cd ../../..
-## sw4lite
-#cd sw4lite
-#mpirun -n $2 --hostfile $HOSTFILE $sw4lite_a_app > $DATASET/$NAME-sw4lite-A.out 2> $DATASET/$NAME-sw4lite-A.err
-#cd ..
-## SWFFT
-#cd SWFFT
-#mpirun -n $2 --hostfile $HOSTFILE $swfft_a_app > $DATASET/$NAME-swfft-A.out 2> $DATASET/$NAME-swfft-A.err
-#mpirun -n $2 --hostfile $HOSTFILE $swfft_b_app > $DATASET/$NAME-swfft-B.out 2> $DATASET/$NAME-swfft-B.err
-#mpirun -n $2 --hostfile $HOSTFILE $swfft_c_app > $DATASET/$NAME-swfft-C.out 2> $DATASET/$NAME-swfft-C.err
-#cd ..
+cd miniAMR/openmp
+mpirun -n $2 --hostfile $HOSTFILE $miniamr_a_app > $DATASET/$NAME-miniamr-A.out 2> $DATASET/$NAME-miniamr-A.err
+cd ../..
+# miniqmc
+cd miniqmc/build
+mpirun -n $2 --hostfile $HOSTFILE $miniqmc_a_app > $DATASET/$NAME-miniqmc-A.out 2> $DATASET/$NAME-miniqmc-A.err
+mpirun -n $2 --hostfile $HOSTFILE $miniqmc_b_app > $DATASET/$NAME-miniqmc-B.out 2> $DATASET/$NAME-miniqmc-B.err
+mpirun -n $2 --hostfile $HOSTFILE $miniqmc_c_app > $DATASET/$NAME-miniqmc-C.out 2> $DATASET/$NAME-miniqmc-C.err
+cd ../..
+# miniVite
+cd miniVite
+mpirun -n $2 --hostfile $HOSTFILE $minivite_a_app > $DATASET/$NAME-minivite-A.out 2> $DATASET/$NAME-minivite-A.err
+cd ..
+# Nekbone
+cd Nekbone/test/example1/
+mpirun -n $2 --hostfile $HOSTFILE $nekbone_a_app > $DATASET/$NAME-nekbone-A.out 2> $DATASET/$NAME-nekbone-A.err
+cd ../../..
+# sw4lite
+cd sw4lite
+mpirun -n $2 --hostfile $HOSTFILE $sw4lite_a_app > $DATASET/$NAME-sw4lite-A.out 2> $DATASET/$NAME-sw4lite-A.err
+cd ..
+# SWFFT
+cd SWFFT
+mpirun -n $2 --hostfile $HOSTFILE $swfft_a_app > $DATASET/$NAME-swfft-A.out 2> $DATASET/$NAME-swfft-A.err
+mpirun -n $2 --hostfile $HOSTFILE $swfft_b_app > $DATASET/$NAME-swfft-B.out 2> $DATASET/$NAME-swfft-B.err
+mpirun -n $2 --hostfile $HOSTFILE $swfft_c_app > $DATASET/$NAME-swfft-C.out 2> $DATASET/$NAME-swfft-C.err
+cd ..
